e18b888838bae22232f5fb111a3e424cf0ebb1b0
s3m <s3m@archlinux.org>
2023-05-25 07:37:29 +0200

    nvim: added keybindings and formatted config

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,7 +1,8 @@
 local lsp = require("lsp-zero").preset({})
 
 lsp.on_attach(function(client, bufnr)
-	lsp.default_keymaps({ buffer = bufnr })
+    lsp.default_keymaps({ buffer = bufnr })
+    vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, { buffer = true })
 end)
 
 lsp.setup()
@@ -12,24 +13,24 @@ local cmp_action = require("lsp-zero").cmp_action()
 require("luasnip.loaders.from_vscode").lazy_load()
 
 cmp.setup {
-	sources = {
-		{ name = "path" },
-		{ name = "nvim_lsp" },
-		{ name = "buffer" },
-		{ name = "luasnip" },
-	},
-	mapping = {
-		["<C-f>"] = cmp_action.luasnip_jump_forward(),
-		["<C-b>"] = cmp_action.luasnip_jump_backward(),
-	}
+    sources = {
+        { name = "path" },
+        { name = "nvim_lsp" },
+        { name = "buffer" },
+        { name = "luasnip" },
+    },
+    mapping = {
+        ["<C-f>"] = cmp_action.luasnip_jump_forward(),
+        ["<C-b>"] = cmp_action.luasnip_jump_backward(),
+    }
 }
 
 require("lspconfig").lua_ls.setup {
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { "vim" }
-			}
-		}
-	}
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { "vim" }
+            }
+        }
+    }
 }
--- .config/nvim/lua/s3m/packer.lua
+++ .config/nvim/lua/s3m/packer.lua
@@ -1,53 +1,44 @@
 vim.cmd("packadd packer.nvim")
 
 return require("packer").startup(function(use)
-	-- Packer
-	use { "wbthomason/packer.nvim" }
+    use { "wbthomason/packer.nvim" }
 
-	-- Telescope
-	use {
-		"nvim-telescope/telescope.nvim", tag = "0.1.1",
-		requires = { { "nvim-lua/plenary.nvim" } }
-	}
+    use {
+        "nvim-telescope/telescope.nvim", tag = "0.1.1",
+        requires = { { "nvim-lua/plenary.nvim" } }
+    }
 
-	-- Colorscheme
-	use { "ellisonleao/gruvbox.nvim" }
+    use { "ellisonleao/gruvbox.nvim" }
 
-	-- Treesitter
-	use {
-		"nvim-treesitter/nvim-treesitter",
-		run = function()
-			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-			ts_update()
-		end,
-	}
+    use {
+        "nvim-treesitter/nvim-treesitter",
+        run = function()
+            local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+            ts_update()
+        end,
+    }
 
-	-- Lsp
-	use {
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v2.x",
-		requires = {
-			-- LSP Support
-			{ "neovim/nvim-lspconfig" }, -- Required
-			{
-				-- Optional
-				"williamboman/mason.nvim",
-				run = function()
-					pcall(vim.cmd, "MasonUpdate")
-				end,
-			},
-			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
+    use {
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v2.x",
+        requires = {
+            { "neovim/nvim-lspconfig" },
+            {
+                "williamboman/mason.nvim",
+                run = function()
+                    pcall(vim.cmd, "MasonUpdate")
+                end,
+            },
+            { "williamboman/mason-lspconfig.nvim" },
 
-			-- Autocompletion
-			{ "hrsh7th/nvim-cmp" }, -- Required
-			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
-			{ "L3MON4D3/LuaSnip" }, -- Required
+            { "hrsh7th/nvim-cmp" },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "L3MON4D3/LuaSnip" },
 
-			-- Extra sources
-			{ "hrsh7th/cmp-buffer" },
-			{ "hrsh7th/cmp-path" },
-			{ "saadparwaiz1/cmp_luasnip" },
-			{ "rafamadriz/friendly-snippets" },
-		}
-	}
+            { "hrsh7th/cmp-buffer" },
+            { "hrsh7th/cmp-path" },
+            { "saadparwaiz1/cmp_luasnip" },
+            { "rafamadriz/friendly-snippets" },
+        }
+    }
 end)
