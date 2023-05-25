f2866094a4dfcd192a9044441177fc70bbfa776d
s3m <s3m@archlinux.org>
2023-05-25 14:44:59 +0200

    nvim: changed telescope keymap to use git_files if in a git repo and else use find_files

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,3 +1,7 @@
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>ff", function()
+    if not pcall(builtin.git_files) then
+        builtin.find_files()
+    end
+end)
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
