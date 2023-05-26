3a44bae7828fd266cb88f491338a3bd6b03815a5
s3m <s3m@archlinux.org>
2023-05-26 12:08:03 +0200

    nvim: made tmux-sessionizer keymap silent

--- .config/nvim/lua/s3m/keymaps.lua
+++ .config/nvim/lua/s3m/keymaps.lua
@@ -20,4 +20,4 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<C-f>", "<cmd>!tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
