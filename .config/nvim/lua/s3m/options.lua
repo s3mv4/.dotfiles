d72729d9dc0c56b31b35aba22ce13b7f010e9611
s3m <s3m@archlinux.org>
2025-05-06 14:36:44 +0200

    nvim: added case options

--- .config/nvim/lua/s3m/options.lua
+++ .config/nvim/lua/s3m/options.lua
@@ -14,6 +14,8 @@ vim.opt.colorcolumn = "80"
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
